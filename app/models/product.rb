46b3c09cd7b37881c9d1a326a98d5b2a28fef65b
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-11-07 15:07:03 +0300

    copmleted code challenge

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(star_rating: star_rating, comment: comment, user_id: user.id, product_id: self.id)
+    end
+    def print_all_reviews
+        puts self.reviews.map{|review|"Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"}
+    end
+    def average_rating
+        #use inbuilt average method for activerecord
+        self.reviews.average(:star_rating).to_f
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_06_205529) do
+ActiveRecord::Schema.define(version: 2022_11_07_100144) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"

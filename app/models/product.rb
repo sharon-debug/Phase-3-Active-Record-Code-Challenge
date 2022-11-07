0b04a6ecb68e723f123a870d4246fbb4d689b0e8
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-11-07 15:53:39 +0300

    Done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,18 @@
 class Product < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(star_rating: star_rating, comment: comment, product_id: id, user_id: user.id)
+    end
+
+    def print_all_reviews
+        self.reviews.each do |review|
+            review.print_review
+        end
+    end
+
+    def average_rating
+        self.reviews.average('star_rating').to_f
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    def favorite_product
+        self.products.order('star_rating').last #returns the product instance
+        ##self.products.maximum('star_rating') returns the highest rating only e.g 8
+    end
+
+    def remove_reviews(product)
+        self.reviews.where(product: product).destroy_all
+    end
 end
